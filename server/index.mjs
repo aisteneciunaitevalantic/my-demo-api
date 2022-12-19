2101cf1867131edd4287723b6f9b3115ef520db2
Aistė Nečiūnaitė <aiste.neciunaite@cec.valantic.com>
2022-12-19 11:15:57 +0200

    init commit

---
+++ server/index.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+// import { MongoClient } from 'mongodb'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+	res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
