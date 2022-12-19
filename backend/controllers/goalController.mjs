c1bb497a7cab90496cda4aba0316a53757c1c0d3
Aistė Nečiūnaitė <aiste.neciunaite@cec.valantic.com>
2022-12-19 13:46:41 +0200

    db connection setup

--- backend/controllers/goalController.mjs
+++ backend/controllers/goalController.mjs
@@ -1,10 +1,12 @@
 import asyncHandler from "express-async-handler";
+import Goal from "../models/goalModel.mjs";
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-	res.json({ message: "get goals" });
+	const goals = await Goal.find();
+	res.json(goals);
 });
 
 // @desc Set goal
@@ -15,21 +17,34 @@ const setGoal = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error("Please add a text field");
 	}
-	res.json({ message: "set goal" });
+	const goal = await Goal.create({ text: req.body.text });
+	res.json(goal);
 });
 
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-	res.json({ message: `Update goal ${req.params.id}` });
+	const goal = await Goal.findById(req.params.id);
+	if (!goal) {
+		res.status(400);
+		throw new Error("Goal not found");
+	}
+	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
+	res.json(updatedGoal);
 });
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-	res.json({ message: `Delete goal ${req.params.id}` });
+	const goal = await Goal.findById(req.params.id);
+	if (!goal) {
+		res.status(400);
+		throw new Error("Goal not found");
+	}
+	await Goal.findByIdAndDelete(req.params.id);
+	res.status(200).send("Deleted");
 });
 
 export { getGoals, setGoal, deleteGoal, updateGoal };
--- backend/server.mjs
+++ backend/server.mjs
@@ -2,9 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import goalRoutes from "./routes/goalRoutes.mjs";
 import { errorHandler } from "./middleware/errorMiddleware.mjs";
+import colors from "colors";
+import connectDb from "./config/db.mjs";
 
 dotenv.config();
 
+connectDb();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
